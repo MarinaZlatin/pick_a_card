bf66d9e2f92e3a427297f5ab8266460931a742e8
Marina Zlatin <zlatin1@bezeqint.net>
2017-10-09 12:13:30 +0300

    Update pick_a_card.rb

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,4 +1,5 @@
 # Курс "Хороший программист-2 Урок 3 Домашнее задание 1"
+puts "Update from GitHub"
 values = ['2','3','4','5','6','7','8',
 '9','10','J','Q','K','A']
 suits = ['Diamonds','Hearts','Clubs','Spades']
